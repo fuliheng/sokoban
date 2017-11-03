06123b2c04faf9342b62df67ee5b5a01f7e1bd84
fuliheng <funono99@gmail.com>
2017-11-03 14:17:55 +0800

    1103-2

--- httpd/index.js
+++ httpd/index.js
@@ -3,18 +3,37 @@
 var http = require('http');
 
 http.createServer((request, response) => {
+  let fs = require('fs');
+  let postData = '';
+  request.on('data',(chunk) =>
+    postData += chunk;
+
+    console.log(
+      '接收的POST DATA 片段Ｋ：['+chunk+'].'
+    );
+) ;
   request.on('end',()=>{
-    console.log('Request method: '+request.method);
-    console.log('Request url: '+ request.url);
-  })
-    let fs = require('fs')
-    fs.readFile('index.html', (err, data) => {
-        response.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        response.write(data);
-        response.end();
-    });
+    switch(request.url){
+      case '/':
+        fs.readFile('../htdocs/index.html',(err.data)=>{
+          if(err){
+            consloe.log('檔案讀取錯誤');
+          }
+          else{
+          response.writeHead(200,{
+          'Content-Type':'text/html'
+          })
+          response.write(data);
+          response.end();
+        }
+      });
+      break;
+    default:
+    consloe.log('未定義的存取：'+request.url);
+    response.end();
+      break;
+    }
+  });
 }).listen(8080);
 // log message to Console
 console.log('Server running at http://127.0.0.1:8080/');
