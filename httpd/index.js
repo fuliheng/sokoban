7498bc2ca9627c02ae2e39cd2b5d4a82a1c7176d
fuliheng <funono99@gmail.com>
2017-11-10 14:19:37 +0800

    1110-2

--- httpd/index.js
+++ httpd/index.js
@@ -1,24 +1,9 @@
 'use strict';
 let http = require('http');
-const routingTable = {
-  '/': {
-    url: '../htdocs/index.html',
-    type: 'text/html'
-  },
-  '/styles.css': {
-    url: '../htdocs/assets/css/styles.css',
-    type: 'text/css'
-  },
-  '/SokobanClone_byVellidragon.png': {
-    url: '../htdocs/assets/png/SokobanClone_byVellidragon.png',
-    type: 'text/css'
-  },
-};
-
 /**
 * 利⽤http.ServerResponse 物件回傳檔案內容
-*
-* @name serve
+**
+@name serve
 * @function
 * @param response - http.ServerResponse 物件
 * @param fname - 要回傳的檔案名
@@ -35,17 +20,15 @@ let serve = (response, fname, datatype) => {
       response.writeHead(200, {
         'Content-Type': datatype
       });
-
       response.write(data);
       response.end();
     }
   });
 };
-
 http.createServer((request, response) => {
   let fs = require('fs');
   let postData = '';
-
+  10
   // 利⽤'data' event 消耗掉data chunk;
   // 'end' event 才會被fired
   request.on('data', (chunk) => {
@@ -55,17 +38,28 @@ http.createServer((request, response) => {
     );
   });
   request.on('end', () => {
-    if (request.url in routingTable) {
-      let obj = routingTable[request.url];
-
-      serve(response, obj.url, obj.type);
-    }
-    else {
+    switch (request.url) {
+      case '/':
+      serve(response, '../htdocs/index.html', 'text/html');
+      break;
+      case '/assets/css/styles.css':
+      serve(response, '../htdocs/assets/css/styles.css', 'text/css');
+      break;
+      case '/assets/png/SokobanClone_byVellidragon.png':
+      serve(
+        response,
+        '../htdocs/assets/png/SokobanClone_byVellidragon.png',
+        'image/png'
+      );
+      break;
+      default:
       console.log(' 未定義的存取: ' + request.url);
       response.end();
+      break;
     }
   });
 }).listen(8088);
 // log message to Console
+11
 console.log(' 伺服器啓動，連線url: http://127.0.0.1:8088/');
 // index.js
